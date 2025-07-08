030e11bb86fbf5cc981c920d3b2c7f67a7bf7d6e
Nickcdub <nickcdub@gmail.com>
2025-07-08 16:41:08 -0700

    updated the update functions

--- models/update-item.js
+++ models/update-item.js
@@ -1,18 +1,18 @@
 const { getCollection } = require('../connect');
 const {ObjectId} = require('mongodb')//
 
-async function updateParam(itemId, field, updatedItem) {
+async function updateParam(itemId, updatedItem) {
     // updateParam(1, Discount, 20) option 2
     const db = getCollection();
     //const documentId = ObjectId.createFromHexString(itemId)
     try {
         const results = await db.updateOne(
             { productId: itemId },
-            { $set: { [field]: updatedItem}},
+            { $set: updatedItem },
             { upsert: false }
         );
         //const results = await db.updateOne({ productID: itemId } , { $set: {Discount: updatedItem}});
-        console.log("Discount updated: ", results);
+        console.log("Update Successful: ", results);
         return results;
     } catch (e) {
         console.log(e);
