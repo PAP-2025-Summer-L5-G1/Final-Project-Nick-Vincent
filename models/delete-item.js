937a0c9196a46b80fd0c2cdebcebafff05f463a6
vinnyly <vincently06@gmail.com>
2025-07-09 17:36:27 -0700

    completed CRUD functionalities for frontend. work on frontend styling and input handling

--- models/delete-item.js
+++ models/delete-item.js
@@ -1,10 +1,13 @@
 const {getCollection} = require('../connect');
+const { ObjectId } = require('mongodb')
 
 // Accepts deleteItem({field : value})
-async function deleteItem(field, value) {
+async function deleteItem(id) {
+    const db = getCollection()
+    console.log("hi we testing delete:", id);
+    const documentId = ObjectId.createFromHexString(id);
     try {
-        const db = getCollection()
-        const res = await db.deleteOne({[field]: value})
+        const res = await db.deleteOne({_id: documentId})
         return res
     } catch (e) {
         console.error(e)
